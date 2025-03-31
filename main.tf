d99917fee5d3849f45cda32c91d6185316f4c30b
rakeshgorai <gorairakesh@gmail.com>
2025-03-31 11:19:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@
 # schemas, see: https://registry.terraform.io/providers/hashicorp/fakewebservices
 #
 # If you're looking for the configuration for the remote backend, you can find that
-# in backend.tf.
+# in backend.tf. test
 
 
 resource "fakewebservices_vpc" "primary_vpc" {
